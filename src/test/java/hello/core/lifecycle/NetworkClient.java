551ab781eb8b6767340bbb337afee1df25e9bb78
nodiary <rohilkie94@gmail.com>
2024-04-13 11:38:58 +0900

    chap 9 빈 생명주기 - 결론 : annotation (@PostConstruct, @PreDestroy)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,8 @@
 package hello.core.lifecycle;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class NetworkClient {
     private String url;
@@ -24,12 +26,13 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close = " + url);
     }
-
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
